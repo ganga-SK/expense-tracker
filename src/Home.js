2b0e083ba0e0f49d00f7c4dfd9b20eaf687aa859
gsk <gangaskumar83@gmail.com>
2024-06-29 16:40:32 +0530

    home.js, navbar.js, index.css

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div className="home-content">
+            Content
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <h1>Expense Tracker</h1>
+            <div 
+        className="links">
+            <a href="/">Home</a>
+            <a href="/create">Add Expense</a>
+        </div>
+        </div>
+        
+     );
+}
+ 
+export default Navbar;
